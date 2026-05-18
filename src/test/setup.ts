2f1b1fe2e761d0aa77a7d83601841cc775420049
Michael Amoabeng Amoabeng <michael.amoabeng2015@gmail.com>
2026-05-19 01:46:30 +0300

    chore: add MSW for API mocking

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,1 +1,9 @@
 import '@testing-library/jest-dom';
+import { afterAll, afterEach, beforeAll } from 'vitest';
+
+import { server } from './msw/server';
+
+// onUnhandledRequest: 'error' surfaces missing mocks loudly instead of letting tests hit the network.
+beforeAll(() => server.listen({ onUnhandledRequest: 'error' }));
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
